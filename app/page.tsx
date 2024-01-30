fcc1966c5d910bd04f1d6205a3b25ec5b545c3f1
You Song Hou <ysongweb3@gmail.com>
2024-01-30 01:21:06 -0500

    feat: add an example of Farcaster Frame

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,29 @@
-import Image from "next/image";
+import { getFrameMetadata } from '@coinbase/onchainkit';
+import type { Metadata } from 'next';
 
-export default function Home() {
+const frameMetadata = getFrameMetadata({
+  buttons: ['Next image'],
+  image: 'https://zizzamia.xyz/park-1.png',
+  post_url: 'https://zizzamia.xyz/api/frame',
+});
+
+export const metadata: Metadata = {
+  title: 'First Frame',
+  description: 'My First Frame',
+  openGraph: {
+    title: 'First Frame',
+    description: 'My First Frame',
+    images: ['https://zizzamia.xyz/park-1.png'],
+  },
+  other: {
+    ...frameMetadata,
+  },
+};
+
+export default function Page() {
   return (
-    <main>
+    <>
       <h1>First Frame</h1>
-    </main>
+    </>
   );
 }
