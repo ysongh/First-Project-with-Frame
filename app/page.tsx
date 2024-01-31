6ff1358fc7f9b354f364b7b8bd092ce67026ec42
You Song Hou <ysongweb3@gmail.com>
2024-01-31 02:20:40 -0500

    feat: add more button

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { getFrameMetadata } from '@coinbase/onchainkit';
 import type { Metadata } from 'next';
 
 const frameMetadata = getFrameMetadata({
-  buttons: ['Next image'],
+  buttons: ['Next image', "More image"],
   image: 'https://images.unsplash.com/photo-1527066236128-2ff79f7b9705?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8d2F0ZXJ8ZW58MHx8MHx8fDA%3D',
   post_url: 'https://zizzamia.xyz/api/frame',
 });
