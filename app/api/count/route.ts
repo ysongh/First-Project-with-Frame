f6d1183a0c6293f555c7a03afb007b7fe49f277c
You Song Hou <ysongweb3@gmail.com>
2024-02-02 01:32:02 -0500

    fix: move the variable 'count' inside the function

--- app/api/count/route.ts
+++ app/api/count/route.ts
@@ -1,9 +1,8 @@
 import { NextRequest, NextResponse } from 'next/server';
 
-export let count = 1;
-
 async function getResponse(req: NextRequest): Promise<NextResponse> {
-  count++;
+  let count = 1;
+  
   return new NextResponse(`<!DOCTYPE html><html><head>
     <meta property="fc:frame" content="vNext" />
     <meta property="fc:frame:image" content="https://images.unsplash.com/photo-1483004406427-6acb078d1f2d?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fHdhdGVyfGVufDB8MHwwfHx8MA%3D%3D" />
