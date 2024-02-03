bfdc088617b6abe57cf0e41593ea6e14bcb33a88
You Song Hou <ysongweb3@gmail.com>
2024-02-03 01:21:04 -0500

    feat: add a Frame with text input

---
+++ app/api/input/route.ts
@@ -0,0 +1,38 @@
+import { FrameRequest, getFrameMessage, getFrameHtmlResponse } from '@coinbase/onchainkit';
+import { NextRequest, NextResponse } from 'next/server';
+
+const NEXT_PUBLIC_URL = 'https://first-frame.onrender.com';
+
+async function getResponse(req: NextRequest): Promise<NextResponse> {
+  let accountAddress: string | undefined = '';
+  let text: string | undefined = '';
+
+  const body: FrameRequest = await req.json();
+  const { isValid, message } = await getFrameMessage(body, { neynarApiKey: 'NEYNAR_ONCHAIN_KIT' });
+
+  if (isValid) {
+    accountAddress = message.interactor.verified_accounts[0];
+  }
+
+  if (body?.untrustedData?.inputText) {
+    text = body.untrustedData.inputText;
+  }
+
+  return new NextResponse(
+    getFrameHtmlResponse({
+      buttons: [
+        {
+          label: `Text: ${text}`,
+        },
+      ],
+      image: "https://images.unsplash.com/photo-1512138664757-360e0aad5132?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjN8fHdhdGVyfGVufDB8fDB8fHww",
+      post_url: `${NEXT_PUBLIC_URL}/api/frame`,
+    }),
+  );
+}
+
+export async function POST(req: NextRequest): Promise<Response> {
+  return getResponse(req);
+}
+
+export const dynamic = 'force-dynamic';
--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,16 @@ import { getFrameMetadata } from '@coinbase/onchainkit';
 import type { Metadata } from 'next';
 
 const frameMetadata = getFrameMetadata({
-  buttons: ['Next image', "More image"],
+  buttons: [
+    {
+      label: 'Click Me',
+    }
+  ],
   image: 'https://images.unsplash.com/photo-1527066236128-2ff79f7b9705?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8d2F0ZXJ8ZW58MHx8MHx8fDA%3D',
-  post_url: 'https://first-frame.onrender.com/api/count',
+  input: {
+    text: 'Tell me a boat story',
+  },
+  post_url: 'https://first-frame.onrender.com/api/input',
 });
 
 export const metadata: Metadata = {
